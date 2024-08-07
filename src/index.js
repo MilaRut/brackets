babc64b91414b11980cc22a6943450d5639382d0
Mila Balashova <mila.balashova2312@gmail.com>
2024-08-08 01:11:01 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let conf = [];
+  bracketsConfig.forEach((el) => {
+    conf.push(el.join(''));
+  })
+
+  let hasChanges;
+
+  do {
+    hasChanges = false;
+
+    for (const pair of conf) {
+
+      let regex = new RegExp(pair, 'g');
+      if (regex == '/()/g') {
+        regex = /\(\)/g;
+      }
+      if (regex == '/{}/g') {
+        regex = /\{\}/g;
+      }
+      if (regex == '/[]/g') {
+        regex = /\[\]/g;
+      }
+      if (regex == '/||/g') {
+        regex = /\|\|/g;
+      }
+      const newStr = str.replace(regex, '');
+      if (newStr !== str) {
+        hasChanges = true;
+        str = newStr;
+      }
+    }
+  } while (hasChanges);
+
+  return str.length === 0;
 }
